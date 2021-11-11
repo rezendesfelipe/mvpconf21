94e4ee3c300c00d9e555af170fc4dd729d6aee3d
Felipe Rezende <felipe.rezende@softwareone.com>
2021-11-11 01:12:23 -0300

    Inserção de NSG

--- demo-04/main.tf
+++ demo-04/main.tf
@@ -17,5 +17,19 @@ module "vnet" {
 
 module "nsg" {
   source = "./module/nsg"
-  
+
+  resource_group_name = module.rg.rg_name
+  location            = module.rg.rg_location
+  nsg_name            = "nsg-testing"
+  tags                = vartags
+  rules = [
+    {
+      name                   = "allow_ssh_in"
+      priority               = 201
+      direction              = "Inbound"
+      destination_port_range = "22"
+      source_address_prefix  = "10.0.1.0/24"
+      description            = "description-myssh"
+    },
+  ]
 }
